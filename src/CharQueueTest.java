88ee06329753eff5c569b75cf65acc1adeabeae3
kenogihara <kenogihara808@gmail.com>
2024-01-27 03:30:39 -0800

    bruh

---
+++ src/CharQueueTest.java
@@ -0,0 +1,61 @@
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class CharQueueTest {
+
+
+    CharQueue queue;
+    CharQueue line;
+    @BeforeEach
+    void setUp() {
+        queue = new CharQueue();
+    }
+
+    @BeforeEach
+    void setUp() { line = new CharQueue(10); }
+
+
+    @Test
+    void constructorOneTest() {
+
+    }
+
+
+    @Test
+    void constructorTwoTest() {
+
+    }
+
+
+
+    @Test
+    void isEmpty() {
+
+        assertTrue()
+    }
+
+    @Test
+    void isFull() {
+
+    }
+
+    @org.junit.jupiter.api.Test
+    void size() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void clear() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void enqueue() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void peek() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void dequeue() {
+    }
